62680cddeabd31b8bfaeb986d2fffd8a7b12ae75
MauriBarrientos <mauribarrientos23@gmail.com>
2023-11-19 13:05:32 -0300

    Aiuda

--- client/src/components/user.jsx
+++ client/src/components/user.jsx
@@ -26,6 +26,14 @@ const UserView = () => {
     };
   }, []);
 
+  // Crea una constante para el LoadScript de Google Maps API
+  const googleMapsApiKey = "AIzaSyAdfrKnsern-zn80h22lDBl00D2z51J_h8"; // Reemplaza 'TU_API_KEY' con tu clave de API de Google Maps
+
+  const mapContainerStyle = {
+    width: '100%',
+    height: '400px',
+  };
+
   const handleShowModalOrigen = () => {
     setShowModalOrigen(true);
   };
@@ -43,20 +51,23 @@ const UserView = () => {
   };
 
   const handleSelectLocation = (location) => {
-  if (!origin) {
-    setOrigin(location);
-    handleCloseModalOrigen(); // Se asegura que el modal se cierre después de seleccionar el origen
-  } else {
-    setDestination(location);
-      // Calcula y muestra las direcciones entre origen y destino
-      setDirections({
-        origin: { lat: origin.lat, lng: origin.lng },
-        destination: { lat: destination.lat, lng: destination.lng },
-        travelMode: 'DRIVING',
-      });
+    if (!origin) {
+      setOrigin(location);
+      handleCloseModalOrigen();
+    } else {
+      if (!destination) {
+        setDestination(location);
+        handleCloseModalDestino();
+      } else {
+        setDestination(location);
+        setDirections({
+          origin: { lat: origin.lat, lng: origin.lng },
+          destination: { lat: location.lat, lng: location.lng },
+          travelMode: 'DRIVING',
+        });
+        handleCloseModalDestino();
+      }
     }
-
-    handleCloseModalDestino();
   };
 
   const handleEnviarCoordenadas = () => {
@@ -73,51 +84,57 @@ const UserView = () => {
 
   return (
     <div>
-<Button className="primary" onClick={handleShowModalOrigen}>
-  Seleccionar Origen
-</Button>
-<Button className="success" onClick={handleShowModalDestino}>
-  Seleccionar Destino
-</Button>
+      <Button className="primary" onClick={handleShowModalOrigen}>
+        Seleccionar Origen
+      </Button>
+      <Button className="success" onClick={handleShowModalDestino}>
+        Seleccionar Destino
+      </Button>
 
-<Modal show={showModalOrigen} onHide={handleCloseModalOrigen}>
-        <Modal.Header closeButton>
-          <Modal.Title>Seleccionar Origen</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <LoadScript googleMapsApiKey="AIzaSyAdfrKnsern-zn80h22lDBl00D2z51J_h8">
+      <LoadScript googleMapsApiKey={googleMapsApiKey}>
+        {/* Modal para seleccionar Origen */}
+        <Modal show={showModalOrigen} onHide={handleCloseModalOrigen}>
+          {/* Contenido del Modal */}
+          <Modal.Header closeButton>
+            <Modal.Title>Seleccionar Origen</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <div style={{ position: 'relative', width: '100%', height: '100%' }}/>
             <GoogleMap
+            mapContainerStyle={mapContainerStyle}
               center={{ lat: 0, lng: 0 }}
               zoom={2}
               onClick={(event) => handleSelectLocation({ lat: event.latLng.lat(), lng: event.latLng.lng() })}
             >
               {origin && <Marker position={{ lat: origin.lat, lng: origin.lng }} />}
             </GoogleMap>
-          </LoadScript>
-        </Modal.Body>
-      </Modal>
+          </Modal.Body>
+        </Modal>
 
-      <Modal show={showModalDestino} onHide={handleCloseModalDestino}>
-        <Modal.Header closeButton>
-          <Modal.Title>Seleccionar Destino</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <LoadScript googleMapsApiKey="AIzaSyAdfrKnsern-zn80h22lDBl00D2z51J_h8">
+        {/* Modal para seleccionar Destino */}
+        <Modal show={showModalDestino} onHide={handleCloseModalDestino}>
+          {/* Contenido del Modal */}
+          <Modal.Header closeButton>
+            <Modal.Title>Seleccionar Destino</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <div style={{ position: 'relative', width: '100%', height: '100%' }}/>
             <GoogleMap
+            mapContainerStyle={mapContainerStyle}
               center={{ lat: 0, lng: 0 }}
               zoom={2}
               onClick={(event) => handleSelectLocation({ lat: event.latLng.lat(), lng: event.latLng.lng() })}
             >
-              {destination && <Marker position={{ lat: destination.lat, lng: destination.lng }} />}
+              {origin && <Marker position={{ lat: origin.lat, lng: origin.lng }} />}
             </GoogleMap>
-          </LoadScript>
-        </Modal.Body>
-      </Modal>
-
-      {directions && (
-        <div>
-          <LoadScript googleMapsApiKey="AIzaSyAdfrKnsern-zn80h22lDBl00D2z51J_h8">
+          </Modal.Body>
+        </Modal>
+        
+        {/* Código para mostrar direcciones */}
+        {directions && (
+          <div>
             <GoogleMap
+              mapContainerStyle={mapContainerStyle}
               center={{ lat: (origin.lat + destination.lat) / 2, lng: (origin.lng + destination.lng) / 2 }}
               zoom={8}
             >
@@ -135,17 +152,18 @@ const UserView = () => {
               />
               {directions && <DirectionsRenderer directions={directions} />}
             </GoogleMap>
-          </LoadScript>
-        </div>
-      )}
+          </div>
+        )}
 
-      {origin && destination && (
-        <div>
-          <Button variant="info" onClick={handleEnviarCoordenadas}>
-            Enviar Coordenadas
-          </Button>
-        </div>
-      )}
+        {/* Botón para enviar coordenadas */}
+        {origin && destination && (
+          <div>
+            <Button variant="info" onClick={handleEnviarCoordenadas}>
+              Enviar Coordenadas
+            </Button>
+          </div>
+        )}
+      </LoadScript>
     </div>
   );
 };
